a358bf70aae19f4c4ad6ff98e1bdd973346ba6bd
Paul Vithayathil <pvithayathil@pvithayathil-0184.local>
2021-09-21 23:42:04 -0600

    prettify

--- kobe-bryant-shot-selection/src/features/build_features.py
+++ kobe-bryant-shot-selection/src/features/build_features.py
@@ -11,7 +11,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 
 load_dotenv(find_dotenv())
-PROCESSED_PATH= os.getcwd()+os.getenv("PROCESSED_PATH")
+PROCESSED_PATH = os.getcwd() + os.getenv("PROCESSED_PATH")
+
 
 def read_preprocessed_data(input_path: str, is_train: bool) -> pd.DataFrame:
     """
@@ -24,13 +25,14 @@ def read_preprocessed_data(input_path: str, is_train: bool) -> pd.DataFrame:
         df:  pre-processed dataframe ready to be made into features
     """
     if is_train:
-        df = pd.read_csv(input_path+"/train.csv", parse_dates=['game_date'])
+        df = pd.read_csv(input_path + "/train.csv", parse_dates=["game_date"])
     else:
-        df = pd.read_csv(input_path+"/test.csv", parse_dates=['game_date'])
+        df = pd.read_csv(input_path + "/test.csv", parse_dates=["game_date"])
 
     return df
 
-def make_features(df: pd.DataFrame)  -> pd.DataFrame:
+
+def make_features(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     Args:
@@ -38,16 +40,20 @@ def make_features(df: pd.DataFrame)  -> pd.DataFrame:
     Returns:
         df_features: dataframe that has features
     """
-    feature_cols = ['combined_shot_type',
-                    'period',
-                    'season',
-                    'shot_zone_basic',
-                    'shot_zone_range'
-                    ]
+    feature_cols = [
+        "combined_shot_type",
+        "period",
+        "season",
+        "shot_zone_basic",
+        "shot_zone_range",
+    ]
     df_features = df[feature_cols]
     return df_features
 
-def make_datasets_model_ready(df_train: pd.DataFrame,df_test: pd.DataFrame)  -> (sparse.csr_matrix, sparse.csr_matrix, list):
+
+def make_datasets_model_ready(
+    df_train: pd.DataFrame, df_test: pd.DataFrame
+) -> (sparse.csr_matrix, sparse.csr_matrix, list):
     """
 
     Args:
@@ -62,39 +68,39 @@ def make_datasets_model_ready(df_train: pd.DataFrame,df_test: pd.DataFrame)  -> 
     """
 
     # define transformers
-    si_0 = SimpleImputer(strategy='constant', fill_value='missing')
+    si_0 = SimpleImputer(strategy="constant", fill_value="missing")
     ohe = OneHotEncoder()
     # define column groups with same processing
-    cat_vars = ['combined_shot_type',
-                'period',
-                'season',
-                'shot_zone_basic',
-                'shot_zone_range'
-                ]
+    cat_vars = [
+        "combined_shot_type",
+        "period",
+        "season",
+        "shot_zone_basic",
+        "shot_zone_range",
+    ]
     # set up pipelines for each column group
-    categorical_pipe = Pipeline([('si_0', si_0), ('ohe', ohe)])
+    categorical_pipe = Pipeline([("si_0", si_0), ("ohe", ohe)])
     # set up columnTransformer
     col_transformer = ColumnTransformer(
-        transformers=[
-            ('cats', categorical_pipe, cat_vars)
-        ],
-        remainder='drop',
-        n_jobs=-1
+        transformers=[("cats", categorical_pipe, cat_vars)], remainder="drop", n_jobs=-1
     )
 
     X_train_transformed = col_transformer.fit_transform(df_train)
     X_test_transformed = col_transformer.transform(df_test)
 
-    col_names =col_transformer.named_transformers_['cats'].named_steps['ohe'] \
-                                .get_feature_names()
+    col_names = (
+        col_transformer.named_transformers_["cats"]
+        .named_steps["ohe"]
+        .get_feature_names()
+    )
 
     return X_train_transformed, X_test_transformed, col_names
 
+
 def main(input_filepath, output_filepath):
-    """
-    """
+    """"""
     logger = logging.getLogger(__name__)
-    logger.info('making features from  training data set from processed data')
+    logger.info("making features from  training data set from processed data")
 
     # Read Data
     df_train = read_preprocessed_data(input_filepath, True)
@@ -105,13 +111,14 @@ def main(input_filepath, output_filepath):
 
     train, test, feature_names = make_datasets_model_ready(df_train, df_test)
 
-    sparse.save_npz(output_filepath+"/train.npz", train)
-    sparse.save_npz(output_filepath+"/test.npz", test)
+    sparse.save_npz(output_filepath + "/train.npz", train)
+    sparse.save_npz(output_filepath + "/test.npz", test)
 
-    with open(output_filepath+'/feature_names.pkl', 'wb') as f:
+    with open(output_filepath + "/feature_names.pkl", "wb") as f:
         pickle.dump(feature_names, f)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     main(PROCESSED_PATH, PROCESSED_PATH)
