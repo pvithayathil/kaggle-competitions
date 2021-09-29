bce93cfc7f62fb53ffe8e23c817f952b75b0c68a
Paul Vithayathil <pvithayathil@pvithayathil-0184.local>
2021-09-28 18:41:10 -0600

    add

---
+++ fake-real-news/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Fake Real News Kaggle DS Problem',
+    author='Paul',
+    license='MIT',
+)
