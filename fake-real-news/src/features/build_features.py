9dd5c3e49d11d21cebba3d58aba3bd06b3c56b34
Paul <paulvithayathils@gmail.com>
2021-10-18 15:09:46 -0600

    99% performance

--- fake-real-news/src/features/build_features.py
+++ fake-real-news/src/features/build_features.py
@@ -3,16 +3,17 @@ from dotenv import find_dotenv, load_dotenv
 import logging
 import numpy as np
 import pandas as pd
-from scipy import sparse
-import pickle
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
+import nltk
+import gensim
+from gensim.utils import simple_preprocess
+from nltk.corpus import stopwords
 
 load_dotenv(find_dotenv())
 PROCESSED_PATH = os.getcwd() + os.getenv("PROCESSED_PATH")
+nltk.download('stopwords')
 
+STOP_WORDS = stopwords.words('english')
+STOP_WORDS.extend(['from', 'subject', 're', 'edu', 'use'])
 
 def read_preprocessed_data(input_path: str, is_train: bool) -> pd.DataFrame:
     """
@@ -31,89 +32,36 @@ def read_preprocessed_data(input_path: str, is_train: bool) -> pd.DataFrame:
 
     return df
 
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2 and token not in STOP_WORDS:
+            result.append(token)
+
+    return result
+
 def make_features(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     Args:
         df: either train or test
     Returns:
-        df_features: dataframe that has features
+        df: dataframe that has features
     """
-    df_features=df
+    # remove stop words
+    df['title_and_text'] = df['title'] + ' ' + df['text']
+    df['clean_title_and_text'] = df['title_and_text'].apply(preprocess)
+    df['clean_joined_title_and_text'] = df['clean_title_and_text'].apply(lambda x: " ".join(x))
 
-    return df_features
+
+    df['clean_title'] = df['title'].apply(preprocess)
+    df['clean_joined_title'] = df['clean_title'].apply(lambda x: " ".join(x))
+
+    df['clean_text'] = df['text'].apply(preprocess)
+    df['clean_joined_text'] = df['clean_text'].apply(lambda x: " ".join(x))
 
 
-def make_datasets_model_ready(
-    df_train: pd.DataFrame, df_test: pd.DataFrame
-) -> (sparse.csr_matrix, sparse.csr_matrix, list):
-    """
-
-    Args:
-        df_train: training data frame
-        df_test: test data frame
-
-    Returns:
-        X_train_transformed: matrix for training features
-        X_test_transformed: matrix for training features
-        col_names: list of feature column names
-
-    """
-
-    # define transformers
-    si_0 = SimpleImputer(strategy="constant", fill_value="missing")
-    ohe = OneHotEncoder(handle_unknown = 'ignore')
-
-    si_1 = SimpleImputer(strategy='mean')
-    mms = MinMaxScaler()
-
-    # define column groups with same processing
-    cat_vars = [
-        "action_type",
-        "period",
-        "playoffs",
-        "season",
-        "opponent",
-        "shot_type",
-        "num_shots_made_in_last_five_attempts",
-        "num_game_last_7_days",
-        "game_month",
-        "loc_bin"
-    ]
-
-    num_vars = [
-        "shot_distance",
-        "num_shots_taken_in_game",
-        "field_goal_pct_in_game",
-        "shot_is_in_possibly_clutch_moment",
-        "is_home_game",
-        "game_is_back_to_back",
-        "made_over_25_points_in_shots"
-    ]
-    # set up pipelines for each column group
-    categorical_pipe = Pipeline([("si_0", si_0), ("ohe", ohe)])
-    numerical_pipe = Pipeline([("si_1", si_1), ("mms", mms)])
-
-    # set up columnTransformer
-    col_transformer = ColumnTransformer(
-        transformers=[
-            ("cats", categorical_pipe, cat_vars),
-            ('nums', numerical_pipe, num_vars),
-        ],
-        remainder="drop", n_jobs=-1
-    )
-
-    X_train_transformed = col_transformer.fit_transform(df_train)
-    X_test_transformed = col_transformer.transform(df_test)
-
-    col_names = (
-        col_transformer.named_transformers_["cats"]
-        .named_steps["ohe"]
-        .get_feature_names()
-    )
-
-    return X_train_transformed, X_test_transformed, col_names
-
+    return df
 
 def main(input_filepath, output_filepath):
     """"""
@@ -127,13 +75,13 @@ def main(input_filepath, output_filepath):
     df_train = make_features(df_train)
     df_test = make_features(df_test)
 
-    train, test, feature_names = make_datasets_model_ready(df_train, df_test)
+    print(df_train.head())
+    print(df_test.head())
 
-    sparse.save_npz(output_filepath + "/train.npz", train)
-    sparse.save_npz(output_filepath + "/test.npz", test)
+    #train, test, feature_names = make_datasets_model_ready(df_train, df_test)
 
-    with open(output_filepath + "/feature_names.pkl", "wb") as f:
-        pickle.dump(feature_names, f)
+    df_train.to_csv(output_filepath + "/train.csv")
+    df_test.to_csv(output_filepath + "/test.csv")
 
 
 if __name__ == "__main__":
